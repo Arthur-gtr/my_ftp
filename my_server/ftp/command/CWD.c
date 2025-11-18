562e2d33ef3f7e4bc9dd7fb6cd14792632eb0c76
Arthur-gtr <arthur.gauthier@epitech.eu>
2025-11-18 02:55:19 +0100

    [FIX] fix the prefix / with cwd and a bug when we need to go back

--- my_server/ftp/command/CWD.c
+++ my_server/ftp/command/CWD.c
@@ -13,18 +13,28 @@
 
 #include <string.h>
 
+void prepend(char *dest, const char *src)
+{
+    size_t len_src = strlen(src);
+    size_t len_dest = strlen(dest);
+    memmove(dest + len_src, dest, len_dest + 1);
+    memcpy(dest, src, len_src);
+}
+
+
 int cwd(ftp_t *ftp, int index, char *command)
 {
     char path_arg[CMD_BUFFER] = {0};
     char new_wc[PATH_MAX] = {0};
 
-    char cwd[4096];
-    getcwd(cwd, sizeof(cwd));
-    printf("Current dir = %s\n", cwd);
     if (is_connected(&ftp->client[CLIENT_IDX(index)], ftp->polling.fds[index].fd) == false)
         return EXIT_SUCCESS;
     get_n_arg(command, path_arg, 2);
     /*Si le path_arg commence par / alors je dois le concaténer par rapport au server->wd*/
+    if ((*path_arg) == '/'){
+        printf("Add pre loc\n");
+        prepend(path_arg, ftp->server.serv_wd);
+    }
     printf("Number of arg == %d\n", get_number_arg(command));
     printf("Path Arg: %s\n", path_arg);
     print_visible(path_arg);
@@ -51,6 +61,7 @@ int cwd(ftp_t *ftp, int index, char *command)
         ftp->client[CLIENT_IDX(index)].wd[count] = new_wc[i];
         count++;
     }
+    ftp->client[CLIENT_IDX(index)].wd[count + 1] = '\0';
     dprintf(ftp->polling.fds[index].fd, "250 Directory successfully changed.\r\n");
     /*je dois get le premier arg dans un buffer*/
     /*On vas faire une simulation du déplacment via chdir puis si il y a une erreur on n' écrase pas pas le path en mémoire*/
